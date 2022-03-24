d68eea6a0657b5ace569fb590e4351134ab690be
Arianit Sylafeta (RIT Student) <as2238@rit.edu>
2022-03-23 21:02:01 -0400

    promises

--- index.js
+++ index.js
@@ -6,48 +6,63 @@ let stocks = {
 }
 
 
-let order  = (Fruit_name, call_production) => {
-    
-    setTimeout(()=> {
-        Output(`${stocks.Fruits[Fruit_name]} was selected`);
-        call_production();
-    }, 1000);
-
-    
-};
-let Output = (instruction) => {
+let render = (instruction) => {
     const list = document.getElementById("list");
     list.innerHTML += `<li>"${instruction}"</li>`;
 };
 
-let production = () => {
-    setTimeout(() => { 
-        Output("production has started");
+let is_shop_open = true;
 
-        setTimeout(() => {
-            Output("Fruit is chopped");
+let order = (time, work) => {
+
+    return new Promise(  (resolve, reject) =>{
+        if(is_shop_open){
+
+            setTimeout( () => {
+                resolve( work())
+            }, time)
             
-            setTimeout(() => {
-                Output(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
+        }else{
+            reject( render("our shop is closed"))
+        }
+    } )
+}
 
-                setTimeout(() => {
-                    Output("Machine was started");
-
-                    setTimeout(() => {
-                        Output(` Icecream was placed on ${stocks.holder[0]}`);
-
-                        setTimeout(() => {
-                            Output(`${stocks.toppings[0]} was added as toppings`);
-                            setTimeout(() => {
-                                Output(``)
-                            })
-
-                        }, 3000)
-                    }, 2000)
-                }, 1000)
-            }, 1000)
-        }, 2000)
-    }, 0000)
-
-};
-
+order(2000, () => {
+    render(`${stocks.Fruits[0]} was selected`)
+})
+.then(() => {
+    return order(0000, () => {
+        render("Production has started");
+    })
+})
+.then(() => {
+    return order(2000, () => {
+        render("The fruit was chopped");
+    })
+})
+.then(() => {
+    return order(1000, () => {
+        render(`${stocks.liquid[0]} and ${stocks.liquid[1]} was selected`)
+    })
+})
+.then(() => {
+    return order(1000, () => {
+        render("start the machine")
+    })
+})
+.then(() => {
+    return order(2000, () => {
+        render(`ice cream was placed on ${stocks.holder[0]} `)
+    })
+})
+.then(() => {
+    return order(3000, () => {
+        render(`${stocks.toppings[0]} was selected`)
+    })
+})
+.then(() => {
+    return order(2000, () => {
+        render("Ice cream was served")
+    })
+})
