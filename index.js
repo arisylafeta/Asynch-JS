ae7acf9faccc44cf984b53aaea0d838105a3be50
Arianit Sylafeta (RIT Student) <as2238@rit.edu>
2022-03-23 21:29:57 -0400

    async await

--- index.js
+++ index.js
@@ -13,56 +13,34 @@ let render = (instruction) => {
 
 let is_shop_open = true;
 
-let order = (time, work) => {
-
-    return new Promise(  (resolve, reject) =>{
+function order(ms){
+    return new Promise((resolve, reject) => {
         if(is_shop_open){
-
-            setTimeout( () => {
-                resolve( work())
-            }, time)
-            
+            setTimeout(resolve, ms)
         }else{
-            reject( render("our shop is closed"))
+            reject(render("Shop is closed"));
         }
-    } )
+    })
+}
+async function kitchen(){
+    try{
+
+        await order(2000);
+        render(`${stocks.Fruits[0]}`);
+        
+        render("start the production");
+        await order(2000)
+        render(`cut the fruit`);
+
+        await order(1000);
+        render(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
+    }
+    catch(error){
+        render("Sorry for seeing you leave", error)
+    }
+    finally{
+        render("day ended, shop is closed")
+    }
 }
 
-order(2000, () => {
-    render(`${stocks.Fruits[0]} was selected`)
-})
-.then(() => {
-    return order(0000, () => {
-        render("Production has started");
-    })
-})
-.then(() => {
-    return order(2000, () => {
-        render("The fruit was chopped");
-    })
-})
-.then(() => {
-    return order(1000, () => {
-        render(`${stocks.liquid[0]} and ${stocks.liquid[1]} was selected`)
-    })
-})
-.then(() => {
-    return order(1000, () => {
-        render("start the machine")
-    })
-})
-.then(() => {
-    return order(2000, () => {
-        render(`ice cream was placed on ${stocks.holder[0]} `)
-    })
-})
-.then(() => {
-    return order(3000, () => {
-        render(`${stocks.toppings[0]} was selected`)
-    })
-})
-.then(() => {
-    return order(2000, () => {
-        render("Ice cream was served")
-    })
-})
+kitchen();
